e82724f17e6cbd4b13a0339880fbfd131d28be1b
TitusEfferian <kiritosato@gmail.com>
2017-06-04 00:03:34 +0700

    first commit

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -158,7 +158,7 @@ public class LineBotController
                             }
                             if(counter)
                             {
-                                getMessageData("current weather on " + part2 + " " + country + " is " + message, idTarget);
+                                getMessageData("current weather on " + part2 + "," + country + " is " + message, idTarget);
                             }
                             else
                             {
