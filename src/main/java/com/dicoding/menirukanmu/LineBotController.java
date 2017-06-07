6e2ad8be24d3aacac2ce3abd32c36f5100e09fcd
TitusEfferian <kiritosato@gmail.com>
2017-06-07 11:21:55 +0700

    first commit

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -424,7 +424,7 @@ public class LineBotController
 
                             }
                             getMessageData("Seller Name: "+seller_name+"\nPositive Rating: "+Integer.toString(positive)+"\nNegative Rating"+Integer.toString(negative)+"\nPrice: Rp. "+Integer.toString(price),idTarget);
-
+                            getMessageData(hasil,idTarget);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
