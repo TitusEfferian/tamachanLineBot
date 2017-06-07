8fd3c308b00bc1393608cb91f3b102b60614c2ea
TitusEfferian <kiritosato@gmail.com>
2017-06-07 11:34:06 +0700

    first commit

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -401,8 +401,12 @@ public class LineBotController
                         while (m.find()) {
 
 
-                            hasil=m.group(1);
-
+                            //hasil=m.group(1);
+                            try {
+                                getMessageData(m.group(1),idTarget);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
                         int price=0;
                         int positive=0;
@@ -424,7 +428,7 @@ public class LineBotController
                                 seller_name=jsonPart.getString("seller_name");
 
                             }
-                            getMessageData("Seller Name: "+seller_name+"\nPositive Rating: "+Integer.toString(positive)+"\nNegative Rating"+Integer.toString(negative)+"\nPrice: Rp. "+Integer.toString(price),idTarget);
+                            //getMessageData("Seller Name: "+seller_name+"\nPositive Rating: "+Integer.toString(positive)+"\nNegative Rating"+Integer.toString(negative)+"\nPrice: Rp. "+Integer.toString(price),idTarget);
                             getMessageData(string,idTarget);
                         } catch (IOException e) {
                             e.printStackTrace();
