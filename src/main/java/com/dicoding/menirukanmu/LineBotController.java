c39d787ee8e246f3d4b2cd38158b047cc82a5233
TitusEfferian <kiritosato@gmail.com>
2017-06-07 10:49:11 +0700

    first commit

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -407,7 +407,7 @@ public class LineBotController
                         try {
                             JSONObject jsonObject = readJsonFromUrl("https://api.bukalapak.com/v2/products.json?keywords=asus%20zenfone%203&page=1&top_seller=1&per_page=1&u=67287:lXymG93y83m6RHzZV5FY");
 
-                            getMessageData(jsonObject.getString("status"),idTarget);
+                            getMessageData(jsonObject.getString("price"),idTarget);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
