1d92adcc7e5d392a069f213a46cbe8a8c2188124
TitusEfferian <kiritosato@gmail.com>
2017-06-08 17:38:12 +0700

    first commit

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -490,8 +490,8 @@ public class LineBotController
                     if(msgText.contains("/debug"))
                     {
                         try {
-                            debug("hai",payload.events[0].source.groupId);
-                            debug("hai juga",payload.events[0].source.groupId);
+                            debug("hai",payload.events[0].replyToken);
+                            debug("hai juga",payload.events[0].replyToken);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
