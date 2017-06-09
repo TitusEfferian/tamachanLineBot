76bde6cc953bb30d93cada07dccc5db545a93620
TitusEfferian <kiritosato@gmail.com>
2017-06-09 18:45:23 +0700

    first commit

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -403,10 +403,10 @@ public class LineBotController {
                         try {
                             JSONObject jsonObject = readJsonFromUrl("http://api.fixer.io/latest?base="+variable1);
                             JSONObject rates = new JSONObject(jsonObject.get("rates").toString());
-                            float f = rates.getInt(variable2.toUpperCase());
+                            Double d = rates.getDouble(variable2.toUpperCase());
 
                            // replyToUser(idTarget,"latest currency on: "+jsonObject.get("date").toString()+"\n"+number+" "+variable1.toUpperCase()+" = "+variable2.toUpperCase()+" "+Double.toString(d*number));
-                            replyToUser(idTarget,Float.toString(f));
+                            replyToUser(idTarget,d.toString());
 
 
                         } catch (IOException e) {
